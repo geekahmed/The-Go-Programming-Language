81a3e57ffc5d3f32acdb3c8cd45362643373b46e
geekahmed <geekahmed1@gmail.com>
2020-05-04 23:56:52 +0200

    Exercise 1.1

---
+++ 01.Tutorial/Exercise-1.1/Exercise-1.1.go
@@ -0,0 +1,16 @@
+/*
+By: Ahmed Moustafa (a.k.a geekahmed)
+Email: geekahmed1@gmail.com
+linkedIn: https://www.linkedin.com/in/geekahmed
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println(os.Args)
+}
