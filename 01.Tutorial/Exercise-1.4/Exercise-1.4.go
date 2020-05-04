4cd17e522885c3b6644784230cc7e4db33ccf3e7
geekahmed <geekahmed1@gmail.com>
2020-05-05 01:00:32 +0200

    Exercise-1.4

--- 01.Tutorial/Exercise-1.4/Exercise-1.4.go
+++ 01.Tutorial/Exercise-1.4/Exercise-1.4.go
@@ -3,3 +3,58 @@ By: Ahmed Moustafa (a.k.a geekahmed)
 Email: geekahmed1@gmail.com
 linkedIn: https://www.linkedin.com/in/geekahmed
 */
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	countsFiles := make(map[string][]string)
+	files := os.Args[1:]
+	if len(files) == 0{
+		countLines(os.Stdin, counts, countsFiles)
+	} else {
+		for _, arg := range files{
+			f, err := os.Open(arg)
+			if err != nil{
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts, countsFiles)
+			f.Close()
+		}
+	}
+
+
+	for line, n := range counts{
+		if n > 1 {
+			fmt.Printf("%d\t%s\t%v\n", n, line, countsFiles[line])
+		}
+	}
+
+}
+
+func countLines (f *os.File, counts map[string]int, countsFiles map[string][]string){
+	input := bufio.NewScanner(f)
+	name := f.Name()
+	for input.Scan(){
+		text := input.Text()
+		counts[text]++
+		if !arrayContains(countsFiles[text], name){
+			countsFiles[text] = append(countsFiles[text], name)
+		}
+	}
+}
+func arrayContains(array []string, value string) bool {
+	for _, item := range array {
+		if item == value {
+			return true
+		}
+	}
+	return false
+}
+
---
+++ 01.Tutorial/Exercise-1.4/README.md
@@ -0,0 +1,1 @@
+**Exercise 1.4:** Modify dup2 to print the names of all files in which each duplicated line occurs.
